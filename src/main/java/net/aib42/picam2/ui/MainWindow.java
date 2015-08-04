8cdf3168b3c2f225d64b9dd5d2b55d517abe739b
aib <aibok42@gmail.com>
2015-08-04 18:39:05 +0300

    Modify code to work with the slicer class from the SDK

--- src/main/java/net/aib42/picam2/ui/MainWindow.java
+++ src/main/java/net/aib42/picam2/ui/MainWindow.java
@@ -4,23 +4,29 @@ import java.awt.BorderLayout;
 import java.awt.Dimension;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 
+import com.example.sony.cameraremote.utils.SimpleLiveviewSlicer;
+import com.example.sony.cameraremote.utils.SimpleLiveviewSlicer.Payload;
+
 import net.aib42.picam2.qx30.Requester;
 
 public class MainWindow implements ActionListener {
-	private Requester req;
+	private SimpleLiveviewSlicer slicer;
+
 	private JPanel mainPanel = new JPanel();
 	private ImagePanel imagePanel = new ImagePanel();
 
 	public MainWindow() throws IllegalStateException, IOException {
-		req = new Requester();
-		req.makeRequest();
-		
+		slicer = new SimpleLiveviewSlicer();
+		slicer.open("http://192.168.8.200:5000/liveview/liveviewstream");
+
 		JFrame frame = new JFrame("picam2");
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -43,12 +49,21 @@ public class MainWindow implements ActionListener {
 
 	public void actionPerformed(ActionEvent e) {
 		if (e.getActionCommand() == "START") {
-			try {
-				imagePanel.update(req.getNextJpegInputStream());
-				mainPanel.repaint();
-			} catch (IOException ex) {
-				ex.printStackTrace(System.err);
-			}
+			new Thread() {
+				@Override
+					public void run() {
+					try {
+						while (true) {
+							Payload pl = slicer.nextPayload();
+							InputStream is = new ByteArrayInputStream(pl.jpegData);
+							imagePanel.update(is);
+							mainPanel.repaint();
+						}
+					} catch (IOException ex) {
+						ex.printStackTrace(System.err);
+					}
+				}
+			}.start();
 		}
 	}
 }
