f264733fc9c550e632d9c95fbb823fe2187e08ac
aib <aibok42@gmail.com>
2015-08-12 21:09:34 +0300

    Remove "Hello, World!" text

--- src/main/java/net/aib42/picam2/ui/Driver.java
+++ src/main/java/net/aib42/picam2/ui/Driver.java
@@ -2,8 +2,6 @@ package net.aib42.picam2.ui;
 
 public class Driver {
 	public static void main(String[] args) {
-		System.out.println("Hello, World!");
-
 		javax.swing.SwingUtilities.invokeLater(new Runnable() {
 			public void run() {
 				try {
